7ce094fe3c0113cee0070a7064551effcaac82ed
Thada Wangthammang <mildronize@gmail.com>
2014-09-17 16:13:24 +0700

    tested simple process

--- r2als/processor.py
+++ r2als/processor.py
@@ -1,6 +1,7 @@
 from r2als import scoring
 import random
 import hashlib
+import copy
 
 class Processor:
 
@@ -13,23 +14,28 @@ class Processor:
     # TABU
     # tabuLists store hash of data
     tabuLists = []
-    tabuSize = 20
+    tabuSize = 0
+
+    def __init__(self, tabuSize=None):
+        if tabuSize is None:
+            self.tabuSize = 20
+        else:
+             self.tabuSize = tabuSize
 
     ################ tabu method ################
     def convertList2String(self, lists):
         strLists =   ",".join(str(x) for x in lists)
         strLists_bytes = strLists.encode('utf-8')
-        # print(strLists_bytes)
         return strLists_bytes
     def hashSolution(self, lists):
-        return hashlib.sha512(self.convertList2String(lists)).hexdigest()
+        return hashlib.sha256(self.convertList2String(lists)).hexdigest()
+
     def addTabuLists(self, data):
-        
         # hash data
         if len(self.tabuLists) <= self.tabuSize:
             self.tabuLists.append(self.hashSolution(data))
         else:
-            self.tabuLists.popleft()
+            self.tabuLists.pop(0)
     def hasTabuLists(self, data):
         hashedData = self.hashSolution(data)
         for tabuList in self.tabuLists:
@@ -39,11 +45,11 @@ class Processor:
 
     ################ scoring method ################
 
-    def getScore(self, data):
-        return scoring.calculate(data)
-    def printScore(self, data):
-        print("The score is "+str(self.getScore(data))+" : ", end=" ")
-        print(data)
+    def getScore(self, lists):
+        return scoring.calculate(lists)
+    def printScore(self, lists):
+        print("The score is "+str(self.getScore(lists))+" : ", end=" ")
+        print(lists)
 
     ################ solution method ################
 
@@ -54,13 +60,18 @@ class Processor:
         random.shuffle(self.workingSolution)
 
     def compareScoreBestSolution(self):
+        #print("compareScoreBestSolution!")
         if scoring.calculate(self.workingSolution) > scoring.calculate(self.bestSolution):
-            self.bestSolution = self.workingSolution
+            print("Get best solution!")
+            self.bestSolution = copy.copy(self.workingSolution)
+            self.printScore(self.bestSolution)
 
     def start(self):
-        print("Started processor")
+        print("Starting processor")
         self.workingSolution = self.loadInitialSolution()
-        self.bestSolution = self.workingSolution
+        self.bestSolution = copy.copy(self.workingSolution)
+        # print(type(self.workingSolution))
+        # print(type(self.bestSolution))
         self.printScore(self.workingSolution)
 
         while self.isWorking:
@@ -71,6 +82,13 @@ class Processor:
             else:
                 self.addTabuLists(self.workingSolution)
             self.compareScoreBestSolution()
-
+            # print("########################")
+            # print("best")
+            # self.printScore(self.bestSolution)
+            # print("working")
+            # self.printScore(self.workingSolution)
+            # input("Press to continue")
             if scoring.calculate(self.bestSolution) >= 8:
                 self.isWorking = False
+
+        print("End processor")
--- r2als/tests/units/test_processor.py
+++ r2als/tests/units/test_processor.py
@@ -13,12 +13,21 @@ class ProcessorTest(unittest.TestCase):
         from r2als import processor
         p = processor.Processor()
         # self.assertEqual(p.hashSolution(b'Hello World'),'2c74fd17edafd80e8447b0d46741ee243b7eb74dd2149a0ab1b9246fb30382f27e853d8585719e0e67cbda0daa8f51671064615d645ae27acb15bfb1447f459b')
-        self.assertEqual(p.hashSolution([0,1,2,3,4,5,6,7,8,9]),'76c7b6bb788fd1d2902c397e8a00cecc5005affb336690b812ae01ce3fb8e609673f674179e3c031b27b840eb3c28a73b70f41c32cfa9da2a3f2a4d6be045144')
+        # print(p.hashSolution([0,1,2,3,4,5,6,7,8,9]))
+        # for sha 256
+        self.assertEqual(p.hashSolution([0,1,2,3,4,5,6,7,8,9]),'f4972c7d08e0c80337aff8d177d66062c2be8a625f71e02b927f3c9aa3a2f48e')
+        # for sha 512
+        #self.assertEqual(p.hashSolution([0,1,2,3,4,5,6,7,8,9]),'8b0ee12df74c1f9ae763d404c3821eb7a7689fa9f78b9c97a867de616e30b4af40b706d241d2b46176745758108c35abd8d204806710ae3a0924d8216aa83540')
 
     def test_convertList2String(self):
         from r2als import processor
         p = processor.Processor()
         self.assertEqual(p.convertList2String([0,1,2,3,4,5,6,7,8,9]),b'0,1,2,3,4,5,6,7,8,9')
+
+    # def test_hasTabuLists(self):
+    #     from r2als import processor
+    #     p = processor.Processor()
+    #
 
     # def test_one_plus_one_should_be_two(self):
     #     self.assertEqual(1+1,2)
