f5cd70e41f1ef7abfb31b5efd265abeecb2bbcc1
Thada Wangthammang <mildronize@gmail.com>
2014-11-05 01:22:02 +0700

    remove class & add root_path , data_path

--- r2als/config.py
+++ r2als/config.py
@@ -3,8 +3,12 @@ config.py
 Description#   Configuration of the system
 Developer#     Thada Wangthammang
 '''
-class Config:
-    db_name = 'r2alsdb'
-    db_name_test = 'r2alsdb_test'
-    host = 'localhost'
-    is_reset = True
+import os
+
+# global variable
+db_name = 'r2alsdb'
+db_name_test = 'r2alsdb_test'
+host = 'localhost'
+is_reset = True
+root_path = os.path.dirname(os.path.dirname(__file__))+'/'
+data_path = root_path + 'data/'
