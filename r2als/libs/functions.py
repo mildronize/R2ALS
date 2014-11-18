52521c94c118025d22af61b4bba2d6f5903bec1f
Thada Wangthammang <mildronize@gmail.com>
2014-11-18 21:39:44 +0700

    add compare semester

--- r2als/libs/functions.py
+++ r2als/libs/functions.py
@@ -2,7 +2,7 @@ import math
 
 class SemesterIndex:
 
-    def __init__(self, num_semester):
+    def __init__(self, num_semester = 0):
         self.num_semester = num_semester
 
     # convert Year & Semester to SemesterIndex
@@ -10,11 +10,23 @@ class SemesterIndex:
         return ((year-1) * self.num_semester + semester) - 1
 
     # convert SemesterIndex to Year
-    def toYear(self, semesterIndex):
-        return math.ceil((semesterIndex + 1) / self.num_semester)
+    def toYear(self, semester_index):
+        return math.ceil((semester_index + 1) / self.num_semester)
 
     # convert SemesterIndex to Semester
-    def toSemester(self, semesterIndex):
-        return semesterIndex + 4 - (3 * self.toYear(semesterIndex))
+    def toSemester(self, semester_index):
+        return semester_index + 4 - (3 * self.toYear(semester_index))
 
-    # def compare semester 
+    def compare_SemesterId(self, semester_id_1, semester_id_2):
+        if semester_id_1.year > semester_id_2.year:
+            return 1
+        elif semester_id_1.year < semester_id_2.year:
+            return -1
+        else:
+            # equal
+            if semester_id_1.semester > semester_id_2.semester:
+                return 1
+            elif semester_id_1.semester < semester_id_2.semester:
+                return -1
+            else:
+                return 0
